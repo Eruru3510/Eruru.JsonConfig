2b29a9a9c50abed4302825ae76b5c958ac256cad
Eruru <1633756198@qq.com>
2026-05-23 01:59:13 +0800

    Add cancel auto reload when save or write

--- src/Eruru.JsonConfig/JsonConfig.cs
+++ src/Eruru.JsonConfig/JsonConfig.cs
@@ -32,7 +32,7 @@ namespace Eruru.JsonConfig {
 		IJsonConfigSource? JsonConfigSource;
 		TConfig? Value;
 		TimeSpan Timeout = TimeSpan.FromSeconds (60);
-		TimeSpan AutoReloadDebouncerTime = TimeSpan.FromSeconds (1);
+		TimeSpan AutoReloadDebouncerTime = TimeSpan.FromMilliseconds (1000);
 		readonly SemaphoreSlim SemaphoreSlim = new (1, 1);
 		int State;
 		int BuildState;
@@ -140,7 +140,7 @@ namespace Eruru.JsonConfig {
 				if (!await TryLoadAsync (cancellationToken).ConfigureAwait (false)) {
 					throw new FileLoadException ("Load json file failed");
 				}
-				await TrySaveAsync (cancellationToken).ConfigureAwait (false);
+				await TrySaveAsync (true, cancellationToken).ConfigureAwait (false);
 				JsonConfigSource.OnChanged += JsonConfigSource_OnChanged;
 				return this;
 			} finally {
@@ -203,7 +203,7 @@ namespace Eruru.JsonConfig {
 			}
 		}
 
-		public async Task<bool> TrySaveAsync (CancellationToken? cancellationToken = null) {
+		public async Task<bool> TrySaveAsync (bool cancelAutoReload = true, CancellationToken? cancellationToken = null) {
 			CheckDisposed ();
 			CheckBuild ();
 			CancellationTokenSource? cancellationTokenSource = null;
@@ -218,7 +218,7 @@ namespace Eruru.JsonConfig {
 				await SemaphoreSlim.WaitAsync (token).ConfigureAwait (false);
 				try {
 					CheckDisposed ();
-					return await TrySaveAsync (Value).ConfigureAwait (false);
+					return await TrySaveAsync (Value, cancelAutoReload).ConfigureAwait (false);
 				} finally {
 					SemaphoreSlim.Release ();
 				}
@@ -226,7 +226,7 @@ namespace Eruru.JsonConfig {
 				cancellationTokenSource?.Dispose ();
 			}
 		}
-		async Task<bool> TrySaveAsync (TConfig? value) {
+		async Task<bool> TrySaveAsync (TConfig? value, bool cancelAutoReload) {
 			var outputStream = JsonConfigSource == null ? null
 				: await JsonConfigSource.OpenOutputStreamAsync ().ConfigureAwait (false);
 			try {
@@ -238,6 +238,11 @@ namespace Eruru.JsonConfig {
 			} finally {
 				if (JsonConfigSource != null) {
 					await JsonConfigSource.CloseOutputStreamAsync (outputStream).ConfigureAwait (false);
+				}
+				if (cancelAutoReload && IsAutoReloadWhenSourceChanged && outputStream != null) {
+					_ = CancelAutoReloadDebouncerAsync ().ContinueWith (static _ => {
+						// TODO: handle exception
+					}, CancellationToken.None, TaskContinuationOptions.RunContinuationsAsynchronously, TaskScheduler.Default);
 				}
 			}
 		}
@@ -281,7 +286,7 @@ namespace Eruru.JsonConfig {
 		}
 
 		public async Task<bool> TryWriteAsync<TState> (
-			Func<JsonConfig<TConfig, TContext>, TConfig, TState, Task> func, TState state
+			Func<JsonConfig<TConfig, TContext>, TConfig, TState, Task> func, TState state, bool cancelAutoReload = true
 			, CancellationToken? cancellationToken = null
 		) {
 #if NET
@@ -314,7 +319,7 @@ namespace Eruru.JsonConfig {
 					}
 					await func (this, value, state).ConfigureAwait (false);
 					jsonConfigOnChangedEventArgs = new (value, Interlocked.Exchange (ref Value, value));
-					return await TrySaveAsync (value).ConfigureAwait (false);
+					return await TrySaveAsync (value, cancelAutoReload).ConfigureAwait (false);
 				} finally {
 					SemaphoreSlim.Release ();
 					if (jsonConfigOnChangedEventArgs != null) {
@@ -326,28 +331,32 @@ namespace Eruru.JsonConfig {
 			}
 		}
 		public Task<bool> TryWriteAsync<TState> (
-			Action<JsonConfig<TConfig, TContext>, TConfig, TState> func, TState state
+			Action<JsonConfig<TConfig, TContext>, TConfig, TState> func, TState state, bool cancelAutoReload = true
 			, CancellationToken? cancellationToken = null
 		) {
 			return TryWriteAsync<(TState, Action<JsonConfig<TConfig, TContext>, TConfig, TState>)> (
 				static (jsonConfig, value, state) => {
 					state.Item2 (jsonConfig, value, state.Item1);
 					return Task.CompletedTask;
-				}, (state, func), cancellationToken
+				}, (state, func), cancelAutoReload, cancellationToken
 			);
 		}
 		public Task<bool> TryWriteAsync (
-			Func<JsonConfig<TConfig, TContext>, TConfig, Task> func, CancellationToken? cancellationToken = null
+			Func<JsonConfig<TConfig, TContext>, TConfig, Task> func, bool cancelAutoReload = true,
+			CancellationToken? cancellationToken = null
 		) {
-			return TryWriteAsync (static (jsonConfig, value, state) => state (jsonConfig, value), func, cancellationToken);
+			return TryWriteAsync (
+				static (jsonConfig, value, state) => state (jsonConfig, value), func, cancelAutoReload, cancellationToken
+			);
 		}
 		public Task<bool> TryWriteAsync (
-			Action<JsonConfig<TConfig, TContext>, TConfig> func, CancellationToken? cancellationToken = null
+			Action<JsonConfig<TConfig, TContext>, TConfig> func, bool cancelAutoReload = true,
+			CancellationToken? cancellationToken = null
 		) {
 			return TryWriteAsync (static (jsonConfig, value, state) => {
 				state (jsonConfig, value);
 				return Task.CompletedTask;
-			}, func, cancellationToken);
+			}, func, cancelAutoReload, cancellationToken);
 		}
 
 		void CheckDisposed () {
@@ -368,9 +377,20 @@ namespace Eruru.JsonConfig {
 			if (Volatile.Read (ref BuildState) == 0 || !IsAutoReloadWhenSourceChanged) {
 				return;
 			}
-			_ = AutoReloadDebouncerAsync ().ContinueWith (
-				static _ => { }, CancellationToken.None, TaskContinuationOptions.RunContinuationsAsynchronously, TaskScheduler.Default
+			_ = AutoReloadDebouncerAsync ().ContinueWith (static _ => {
+				// TODO: handle exception
+			}, CancellationToken.None, TaskContinuationOptions.RunContinuationsAsynchronously, TaskScheduler.Default);
+		}
+
+		async Task CancelAutoReloadDebouncerAsync () {
+			await Task.Delay (TimeSpan.FromTicks (AutoReloadDebouncerTime.Ticks / 2)).ConfigureAwait (false);
+			var oldCancellationTokenSource = Interlocked.CompareExchange (
+				ref AutoReloadDebouncerCancellationTokenSource, null, null
 			);
+			if (oldCancellationTokenSource == null) {
+				return;
+			}
+			await CancelCancellationTokenSourceAsync (oldCancellationTokenSource).ConfigureAwait (false);
 		}
 
 		async Task AutoReloadDebouncerAsync () {
@@ -379,12 +399,7 @@ namespace Eruru.JsonConfig {
 				ref AutoReloadDebouncerCancellationTokenSource, cancellationTokenSource
 			);
 			if (oldCancellationTokenSource != null) {
-#if NET
-				await oldCancellationTokenSource.CancelAsync ().ConfigureAwait (false);
-#else
-				oldCancellationTokenSource.Cancel ();
-#endif
-				oldCancellationTokenSource.Dispose ();
+				await CancelCancellationTokenSourceAsync (oldCancellationTokenSource).ConfigureAwait (false);
 			}
 			try {
 				await Task.Delay (AutoReloadDebouncerTime, cancellationTokenSource.Token).ConfigureAwait (false);
@@ -394,6 +409,15 @@ namespace Eruru.JsonConfig {
 				return;
 			}
 			await TryLoadAsync ().ConfigureAwait (false);
+		}
+
+		static async Task CancelCancellationTokenSourceAsync (CancellationTokenSource cancellationTokenSource) {
+#if NET
+			await cancellationTokenSource.CancelAsync ().ConfigureAwait (false);
+#else
+			cancellationTokenSource.Cancel ();
+#endif
+			cancellationTokenSource.Dispose ();
 		}
 
 	}
